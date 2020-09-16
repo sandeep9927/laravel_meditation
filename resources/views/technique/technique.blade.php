acb43559f84a27fa528279e2660fd98fa095ba8e
sandeep9927 <sandeepsaini24398@gmail.com>
2020-09-16 17:32:03 +0530

    some changes

--- resources/views/technique/technique.blade.php
+++ resources/views/technique/technique.blade.php
@@ -5,12 +5,9 @@
 
 
 <link rel="stylesheet" href="{{ asset('css/ul.css') }}">
-<div class="jumbotron" style="background-image: url('/images/v.jpg')">
- 
-  <div class="container text-center">
-    <h1>Meditation</h1>      
-   
-  </div>
+<div class="jumbotron" style="background-image: url('/images/v.jpg');  background-repeat: no-repeat;
+background-size: 100%;height:250px;">
+
 </div>
 <h4 style="text-align: center">Select Techniques By Tradition or By Need or By Level</h4><hr>
 <div class="container">    
